90c4d8b56cb4cfb0487551728cd7c6077a3cfae9
cscppkotlin@gmail.com <cscppkotlin@gmail.com>
2024-04-24 14:29:00 +0300

    +буфер обмена

--- ContextMenu/Form1.Designer.cs
+++ ContextMenu/Form1.Designer.cs
@@ -28,11 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             listBox1 = new ListBox();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            копироватьToolStripMenuItem = new ToolStripMenuItem();
+            вставитьToolStripMenuItem = new ToolStripMenuItem();
+            вырезатьToolStripMenuItem = new ToolStripMenuItem();
+            contextMenuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // listBox1
             // 
+            listBox1.ContextMenuStrip = contextMenuStrip1;
             listBox1.Dock = DockStyle.Fill;
             listBox1.FormattingEnabled = true;
             listBox1.Items.AddRange(new object[] { "635326fhdfh", "623781jhdjsakj", "86qjaj", "72638hgdjhasd" });
@@ -40,6 +47,35 @@
             listBox1.Name = "listBox1";
             listBox1.Size = new Size(800, 450);
             listBox1.TabIndex = 0;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            listBox1.MouseUp += listBox1_MouseUp;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { копироватьToolStripMenuItem, вставитьToolStripMenuItem, вырезатьToolStripMenuItem });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(163, 76);
+            // 
+            // копироватьToolStripMenuItem
+            // 
+            копироватьToolStripMenuItem.Name = "копироватьToolStripMenuItem";
+            копироватьToolStripMenuItem.Size = new Size(162, 24);
+            копироватьToolStripMenuItem.Text = "Копировать";
+            копироватьToolStripMenuItem.Click += копироватьToolStripMenuItem_Click;
+            // 
+            // вставитьToolStripMenuItem
+            // 
+            вставитьToolStripMenuItem.Name = "вставитьToolStripMenuItem";
+            вставитьToolStripMenuItem.Size = new Size(162, 24);
+            вставитьToolStripMenuItem.Text = "Вставить";
+            вставитьToolStripMenuItem.Click += вставитьToolStripMenuItem_Click;
+            // 
+            // вырезатьToolStripMenuItem
+            // 
+            вырезатьToolStripMenuItem.Name = "вырезатьToolStripMenuItem";
+            вырезатьToolStripMenuItem.Size = new Size(162, 24);
+            вырезатьToolStripMenuItem.Text = "Вырезать";
             // 
             // Form1
             // 
@@ -49,11 +85,16 @@
             Controls.Add(listBox1);
             Name = "Form1";
             Text = "Form1";
+            contextMenuStrip1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private ListBox listBox1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem копироватьToolStripMenuItem;
+        private ToolStripMenuItem вставитьToolStripMenuItem;
+        private ToolStripMenuItem вырезатьToolStripMenuItem;
     }
 }
--- ContextMenu/Form1.cs
+++ ContextMenu/Form1.cs
@@ -6,5 +6,36 @@ namespace ContextMenu
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(MousePosition, ToolStripDropDownDirection.Right);
+            }
+        }
+
+        private void ÍÓÔËÓ‚‡Ú¸ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Clipboard.SetText(listBox1.SelectedItem.ToString());
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+
+        private void ‚ÒÚ‡‚ËÚ¸ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(Clipboard.GetText());
+        }
     }
 }
